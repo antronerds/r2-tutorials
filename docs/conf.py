ab69c0a824b14bdf10f85faffa599adb886900cb
pietmolenaar <piet.amc@gmail.com>
2016-08-18 11:02:28 +0200

    Update conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -10,7 +10,7 @@ copyright = u'2016, Jan Koster'
 author = u'R2 support team'
 
 version = datetime.date.today().strftime('%Y-%m-%d')
-release = '3.0.0.w'
+release = '3.0.0.x'
 language = None
 
 exclude_patterns = ['_build']
