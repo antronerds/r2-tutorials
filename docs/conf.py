97c7c04c15839060ffb0364881819b5479556efd
rvolckmann <volckmann@gmail.com>
2017-04-13 13:25:02 +0200

    Update conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -10,7 +10,7 @@ copyright = u'2016, Jan Koster'
 author = u'R2 support team'
 
 version = datetime.date.today().strftime('%Y-%m-%d')
-release = '3.0.0'
+release = '3.1.0'
 language = None
 
 exclude_patterns = ['_build']
