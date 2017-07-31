3cf75d88e9fcb7813b6faa2d42a2abf09eb54f6e
rvolckmann <volckmann@gmail.com>
2017-07-31 14:39:31 +0200

    Update conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -10,7 +10,7 @@ copyright = u'2016, Jan Koster, http://r2.amc.nl - http://r2platform.com'
 author = u'R2 support team'
 
 version = datetime.date.today().strftime('%Y-%m-%d')
-release = '3.1.1'
+release = '3.1.2'
 language = None
 
 exclude_patterns = ['_build']
