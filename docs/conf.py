665aedac74a468ae015320827926be6c7622b8fa
Lieke <llhoyng.aumc@gmail.com>
2026-09-18 16:11:58 +0200

    update yaml

--- docs/conf.py
+++ docs/conf.py
@@ -3,6 +3,8 @@ import datetime
 extensions = ['myst_parser', 'sphinx_markdown_tables', 'sphinx_design', 'sphinx_copybutton']
 myst_enable_extensions = ["breaks","linkify", "colon_fence"]
 myst_commonmark_only = False
+myst_heading_anchors = 3
+myst_all_links_external = True
 templates_path = ['_templates']
 
 master_doc = 'index'
